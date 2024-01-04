9e1e2f14097603daac03e08dcf6350025e7f6e99
GBalatti9 <gas.balatti@gmail.com>
2024-01-04 20:27:27 -0300

    Index file to export pages

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,9 +2,10 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 import { Navbar } from "../ui";
 
-import { MarvelPage } from "../heroes/pages/MarvelPage";
-import { DcPage } from "../heroes/pages/DcPage";
+
+
 import { LoginPage } from "../auth/pages/LoginPage";
+import { DcPage, MarvelPage } from "../heroes";
 
 
 export const AppRouter = () => {
