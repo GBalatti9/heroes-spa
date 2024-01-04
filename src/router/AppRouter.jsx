e05df33ec4962526af0bc0bb4f8dc7c4456fc836
GBalatti9 <gas.balatti@gmail.com>
2024-01-04 20:25:31 -0300

    NavBar configuration with args in style

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,18 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { MarvelPage } from "../heroes/pages/MarvelPage"
-import { DcPage } from "../heroes/pages/DcPage"
-import { LoginPage } from "../auth/pages/LoginPage"
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import { Navbar } from "../ui";
+
+import { MarvelPage } from "../heroes/pages/MarvelPage";
+import { DcPage } from "../heroes/pages/DcPage";
+import { LoginPage } from "../auth/pages/LoginPage";
 
 
 export const AppRouter = () => {
     return (
         <>
+
+            <Navbar />
+
             <Routes>
                 <Route path="marvel" element={ <MarvelPage /> }/>
                 <Route path="dc" element={ <DcPage /> }/>
