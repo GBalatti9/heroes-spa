509e3e24f839c27bbbaa96b464d4c15d1b74dd89
GBalatti9 <gas.balatti@gmail.com>
2024-01-04 23:12:06 -0300

    Navigate -1 if the user is in the hero detail

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,11 +1,17 @@
-import { Navigate, useParams } from "react-router-dom"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroeById } from "../helpers";
 
 
 export const HeroPage = () => {
-
+    
     const { id } = useParams();
+    const navigate = useNavigate();
+
     const hero = getHeroeById( id );
+
+    const handleNavigateBack = () => {
+        return navigate( -1 );
+    }
 
     if ( !hero ) {
         return <Navigate to='/marvel' />;
@@ -14,6 +20,31 @@ export const HeroPage = () => {
     
     return (
         <>
+            <div className="row mt-5">
+                <div className="col-4">
+                    <img 
+                        src={`/assets/heroesImg/${ id }.jpg`} 
+                        alt={ hero.superhero }
+                        className="img-thumbnail" />
+                </div>
+                <div className="col-8">
+                    <h3>{ hero.superhero }</h3>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item"> <b> Alter ego: </b> { hero.alter_ego } </li>
+                        <li className="list-group-item"> <b> Publisher: </b> { hero.publisher } </li>
+                        <li className="list-group-item"> <b> First appearance: </b> { hero.first_appearance } </li>
+                    </ul>
+
+                    <h5 className="mt-3"> Characters </h5>
+                    <p>{ hero.characters }</p>
+
+                    <button 
+                        className="btn btn-outline-primary"
+                        onClick={ handleNavigateBack }>
+                        Go back
+                    </button>
+                </div>
+            </div>
             <h1>{ hero.superhero }</h1>
         </>
     )
