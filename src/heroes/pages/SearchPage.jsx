b4e178f5f289b74af900fcbe208e4fc4bd21c0b0
GBalatti9 <gas.balatti@gmail.com>
2024-01-05 09:38:33 -0300

    show search message if nothing is search and show error if no heroes are found

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -14,14 +14,15 @@ export const SearchPage = () => {
 
     const heroes = getHeroesByName( q );
 
+    const showSearch = (q.length === 0);
+    const showError  = (q.length > 0) && heroes.length === 0;
+
     const { searchText, handleInputChange } = useForm({
         searchText: q
     });
 
     const handleSearchSubmit = ( e ) => {
         e.preventDefault();
-
-        if ( searchText.trim().length <= 1 ) return;
 
         navigate(`?q=${ searchText }`)
 
@@ -59,12 +60,20 @@ export const SearchPage = () => {
                         <h4>Results</h4>
                         <hr />
 
-                        {
+                        {/* {
                             // If q is empty then show 'Search a hero' message. Else asks if heroes.length is equal to 0, if that is the case then show 'No hero found' message.
                             ( q === '' )
                             ? <div className="alert alert-primary"> Search a hero </div>
                             : ( heroes.length === 0 ) && <div className="alert alert-danger"> No hero found </div>
-                        }
+                        } */}
+                        
+                        <div className="alert alert-primary animate__animated animate__fadeIn" style={{ display: showSearch ? '' : 'none' }}> 
+                            Search a hero 
+                        </div>
+
+                        <div className="alert alert-danger animate__animated animate__fadeIn" style={{ display: showError ? '' : 'none' }}> 
+                            No hero found 
+                        </div>
 
                         {
                             heroes.map(( hero ) => (
