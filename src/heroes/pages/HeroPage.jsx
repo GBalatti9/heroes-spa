6a37f3d91b504dcb2592239eef26123fb8d00a8b
GBalatti9 <gas.balatti@gmail.com>
2024-01-04 23:04:00 -0300

    Navigate to a different view id the product is not found by id

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Navigate, useParams } from "react-router-dom"
+import { getHeroeById } from "../helpers";
+
 
 export const HeroPage = () => {
+
+    const { id } = useParams();
+    const hero = getHeroeById( id );
+
+    if ( !hero ) {
+        return <Navigate to='/marvel' />;
+        
+    }
+    
     return (
-        <div>HeroPage</div>
+        <>
+            <h1>{ hero.superhero }</h1>
+        </>
     )
 }
