68045e00d5536086c450f8f3d58c076d070b8c6c
GBalatti9 <gas.balatti@gmail.com>
2024-01-05 08:56:16 -0300

    SearchPage styling

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,46 @@
-
+import { HeroCard } from '../components';
 
 export const SearchPage = () => {
     return (
-        <div>SearchPage</div>
+        <>
+            <h1>Seaarch</h1>
+            <hr />
+
+            <div className="row">
+
+                <div className="col-5">
+                    <h4>Searching</h4>
+                    <hr />
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Search a hero"
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
+                        />
+
+                        <button className="btn btn-outline-primary mt-1">
+                            Search
+                        </button>
+                    </form>
+
+                </div>
+                    <div className="col-7">
+                        <h4>Results</h4>
+                        <hr />
+
+                        <div className="alert alert-primary">
+                            Search a hero
+                        </div>
+
+                        <div className="alert alert-danger">
+                            No hero found
+                        </div>
+
+                        {/* <HeroCard /> */}
+                </div>
+            </div>
+        </>
     )
 }
