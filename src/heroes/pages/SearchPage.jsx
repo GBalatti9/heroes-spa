bf14f8dfe49dcce261f46979a983132b220f737f
GBalatti9 <gas.balatti@gmail.com>
2024-01-05 09:13:37 -0300

    useNavigate-useLocation-query-stirng library

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,31 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
+
+import { useForm } from '../../hooks/useForm';
 import { HeroCard } from '../components';
 
 export const SearchPage = () => {
+
+    const navigate = useNavigate();
+    const { search } = useLocation();
+    
+    const { q = '' } = queryString.parse( search );
+    console.log({ q });
+
+    const { searchText, handleInputChange } = useForm({
+        searchText: ''
+    });
+
+    const handleSearchSubmit = ( e ) => {
+        e.preventDefault();
+
+        if ( searchText.trim().length <= 1 ) return;
+
+        navigate(`?q=${ searchText.toLowerCase().trim() }`)
+
+        console.log({ searchText });
+    }
+
     return (
         <>
             <h1>Seaarch</h1>
@@ -11,13 +36,15 @@ export const SearchPage = () => {
                 <div className="col-5">
                     <h4>Searching</h4>
                     <hr />
-                    <form>
+                    <form onSubmit={ handleSearchSubmit }>
                         <input
                             type="text"
                             placeholder="Search a hero"
                             className="form-control"
                             name="searchText"
                             autoComplete="off"
+                            value={ searchText }
+                            onChange={ handleInputChange }
                         />
 
                         <button className="btn btn-outline-primary mt-1">
